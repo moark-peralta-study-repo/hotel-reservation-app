708de0188b5ebc7288ec6ad70a599e66633d5e77
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-12-14 11:41:54 +0800

    change bgcolor for toppanel

--- app/src/main/java/org/hotel/view/TopPanel.java
+++ app/src/main/java/org/hotel/view/TopPanel.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class TopPanel extends JPanel {
   public TopPanel(String username) {
     setLayout(new BorderLayout());
-    setBackground(Color.decode("#f9fafb"));
+    setBackground(Color.decode("#ffffff"));
     // setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
     setPreferredSize(new Dimension(0, 80));
     setBorder(BorderFactory.createLineBorder(Color.decode("#f3f4f6")));
