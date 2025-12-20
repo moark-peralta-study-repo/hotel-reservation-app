b9374448d8615af36e7fd20d44db32abb77ec7e2
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-12-20 12:36:53 +0800

    add customerDAO methods

--- app/src/main/java/org/hotel/model/dao/CustomerDAO.java
+++ app/src/main/java/org/hotel/model/dao/CustomerDAO.java
@@ -12,15 +12,18 @@ import org.hotel.db.Database;
 import org.hotel.model.Customer;
 
 public class CustomerDAO {
+
   public void insert(Customer customer) {
-    String sql = "INSERT INTO customer (name, phone, email)";
+    String sql = "INSERT INTO customers (name, phone, email) VALUES (?, ?, ?)";
 
     try (Connection conn = Database.getConnection();
         PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
       pstmt.setString(1, customer.getName());
       pstmt.setString(2, customer.getPhone());
-      pstmt.setString(1, customer.getEmail());
+      pstmt.setString(3, customer.getEmail());
+
+      pstmt.executeUpdate();
     } catch (SQLException e) {
       e.printStackTrace();
     }
@@ -28,22 +31,63 @@ public class CustomerDAO {
 
   public List<Customer> getAll() {
     List<Customer> customers = new ArrayList<>();
-    String sql = "SELECT * FROM customers";
+    String sql = "SELECT * FROM customers WHERE is_active = 1";
 
     try (Connection conn = Database.getConnection();
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery(sql)) {
 
       while (rs.next()) {
-        rs.getInt("id");
-        rs.getString("name");
-        rs.getString("phone");
-        rs.getString("email");
+        Customer customer = new Customer(
+            rs.getInt("id"),
+            rs.getString("name"),
+            rs.getString("phone"),
+            rs.getString("email"));
+
+        customers.add(customer);
       }
+
     } catch (SQLException e) {
       e.printStackTrace();
     }
 
     return customers;
+  }
+
+  public void deactivate(int customerId) {
+    String sql = "UPDATE customers SET is_active = 0 WHERE id = ?";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+      stmt.setInt(1, customerId);
+      stmt.executeUpdate();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Customer getById(int id) {
+    String sql = "SELECT * FROM customers WHERE id = ? AND is_active = 1";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql)) {
+      stmt.setInt(1, id);
+      ResultSet rs = stmt.executeQuery();
+
+      if (rs.next()) {
+        return new Customer(
+            rs.getInt("id"),
+            rs.getString("name"),
+            rs.getString("phone"),
+            rs.getString("email"));
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return null;
   }
 }
