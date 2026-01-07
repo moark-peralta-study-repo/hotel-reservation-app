412ae40da796e6d2fe0bfec275c9990153d0c6b5
moark-peralta-study-repo <markperaltaasd@gmail.com>
2026-01-07 13:57:03 +0800

    add mapper that maps booking to booking dto

--- app/src/main/java/org/hotel/utils/BookingUtils.java
+++ app/src/main/java/org/hotel/utils/BookingUtils.java
@@ -3,6 +3,7 @@ package org.hotel.utils;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.hotel.dto.BookingRowDTO;
 import org.hotel.model.Booking;
 import org.hotel.model.BookingStatus;
 
@@ -20,5 +21,16 @@ public class BookingUtils {
         rs.getString("check_out"),
         rs.getDouble("total_price"),
         status);
+  }
+
+  public static BookingRowDTO mapRowToDTO(ResultSet rs) throws SQLException {
+    return new BookingRowDTO(
+        rs.getInt("booking_id"),
+        rs.getString("customer_name"),
+        rs.getInt("room_number"),
+        rs.getString("check_in"),
+        rs.getString("check_out"),
+        rs.getDouble("total_price"),
+        BookingStatus.valueOf(rs.getString("status")));
   }
 }
