12c66d38b252ad1072a954c11ec25dcef3d79537
moark-peralta-study-repo <markperaltaasd@gmail.com>
2026-01-12 21:25:28 +0800

    initial dao

--- app/src/main/java/org/hotel/model/dao/DashboardDAO.java
+++ app/src/main/java/org/hotel/model/dao/DashboardDAO.java
@@ -1,5 +1,62 @@
 package org.hotel.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.hotel.db.Database;
+
 public class DashboardDAO {
 
+  private int count(String sql) {
+    try (
+        Connection conn = Database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery()
+
+    ) {
+
+      if (rs.next())
+        return rs.getInt(1);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return 0;
+  }
+
+  public int getReservationCount() {
+    return count("""
+          SELECT COUNT(*)
+          FROM bookings
+          WHERE status = 'RESERVED'
+        """);
+  }
+
+  public int getCheckedInCount() {
+    return count("""
+          SELECT COUNT(*)
+          FROM bookings
+          WHERE status = 'CHECKED_IN'
+        """);
+  }
+
+  public int getTodayCheckInCount() {
+    return count("""
+          SELECT COUNT(*)
+          FROM bookings
+          WHERE status = 'RESERVED'
+          AND check_in = date('now')
+        """);
+  }
+
+  public int getTodayCheckOutCount() {
+    return count("""
+          SELECT COUNT(*)
+          FROM bookings
+          WHERE status = 'CHECKED_IN'
+          AND check_out = date('now')
+        """);
+  }
 }
