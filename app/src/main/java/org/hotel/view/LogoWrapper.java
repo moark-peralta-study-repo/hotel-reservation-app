8bf1c04e667b3a5f445a1bb75372839ace375344
moark-peralta-study-repo <markperaltaasd@gmail.com>
2026-01-29 19:11:13 +0800

    add logo and logo wrapper

---
+++ app/src/main/java/org/hotel/view/LogoWrapper.java
@@ -0,0 +1,33 @@
+package org.hotel.view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.net.URL;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LogoWrapper extends JPanel {
+  public LogoWrapper() {
+    setLayout(new BorderLayout());
+    setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+    setBackground(Color.decode("#f9fafb"));
+
+    URL url = getClass().getClassLoader().getResource("icons/logo.png");
+    System.out.println(url);
+
+    ImageIcon logo = new ImageIcon(url);
+
+    Image img = logo.getImage().getScaledInstance(200, 220, Image.SCALE_SMOOTH);
+    logo = new ImageIcon(img);
+
+    JLabel imageLbl = new JLabel(logo);
+    add(imageLbl, BorderLayout.CENTER);
+
+    setPreferredSize(new Dimension(200, 150));
+  }
+}
